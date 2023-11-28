78413c4664c68f9162233c4842dd702fbd8c788e
Kamran Chitsaz <kmchiti@Kamrans-MacBook-Pro.local>
2023-11-27 21:10:28 -0500

    fix bug in flash-atten model. without residual in fp32 model couldn't converge at all.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=molgpt
 #SBATCH --time=0-03:00
 
-#SBATCH --partition=short-unkillable    # ask for unkillable job
+#SBATCH --partition=unkillable    # ask for unkillable job
 #SBATCH --gres=gpu:a100l:1
 #SBATCH --cpus-per-task=4              # number of cpus per gpu
 #SBATCH --mem-per-gpu=32G               # memory per gpu
