47d1535434a2291f349be0c2f9d18e3c42ddf2d2
Kamran Chitsaz <kmchiti@Kamrans-MacBook-Pro.local>
2024-05-15 06:08:59 -0400

    update finetune with SFT

--- finetune.py
+++ finetune.py
@@ -1,84 +1,131 @@
-from eval import get_all_metrics, generate_smiles_HF, generate_smiles_FA
-import hydra
-from omegaconf import DictConfig, OmegaConf
-from transformers import set_seed, Trainer, TrainingArguments
+from hydra import compose, initialize
 from trainer import MyHFTrainer, MyTrainingArguments
 from callbacks import WandbCallback
 from dataset import MolGenDataModule
 from models import GPT2MolGen, GPT2MolGen_flash_atten, Llama_small_flash_atten
 from utils import creat_unique_experiment_name, is_world_process_zero, save_HF_model
+from omegaconf import DictConfig, OmegaConf
 import torch
 import wandb
+import argparse
 from transformers import LlamaForCausalLM, LlamaConfig
 from flash_attn.models.gpt import GPTLMHeadModel
 from tabulate import tabulate
 import pandas as pd
 from eval import generate_smiles_HF, generate_smiles_FA, get_all_metrics
 import os
+from datasets import Dataset, DatasetDict
+from pandarallel import pandarallel
+from generated_and_filter import get_qed, get_sa, get_logp
+
+def args_parser():
+    parser = argparse.ArgumentParser(
+        description='Molecules Generation',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--exp_name', default="llama_small_FA_ZINC_270M_3d67f607_", type=str,
+                        help='name of the trained config')
+    parser.add_argument('--config_name', default="finetune_ZINC_270M_atomwise", type=str,
+                        help='name of the trained config')
+    parser.add_argument('--task', default='qed', type=str, help='task to filter for')
+    args = parser.parse_args()
+    return args
 
 
-@hydra.main(version_base=None, config_path="configs", config_name="config_moses_finetune")
-def entrypoint(cfg: DictConfig):
+def entrypoint(args):
     # Initialize setup
-    set_seed(cfg.seed)
-    exp_name = creat_unique_experiment_name(cfg)
+    with initialize(version_base=None, config_path="configs"):
+        cfg = compose(config_name=args.config_name)
+
+    # Initialize setup
+    exp_name = args.exp_name
     output_dir = os.path.join(cfg.save_path, exp_name)
     os.makedirs(output_dir, exist_ok=True)
 
     # Initialize DataModule
     datamodule = MolGenDataModule(**cfg.dataset)
-    datamodule.setup()
+    df = pd.read_csv(f'{output_dir}/generated_smiles_qed_42.csv')
+    dataset = Dataset.from_dict(df)
+    dataset = DatasetDict({"train": dataset})
 
-    # Load model
+    def tokenize_function(
+            element: dict,
+            max_length: int,
+            mol_type: str,
+            tokenizer,
+    ):
+        """Tokenize a single element of the dataset.
+
+        Args:
+            element (dict): Dictionary with the data to be tokenized.
+            max_length (int): Maximum length of the tokenized sequence.
+            mol_type (str): mol_type of the dataset to be tokenized.
+            tokenizer (PreTrainedTokenizerFast): Tokenizer to be used.
+
+        Returns:
+            dict: Dictionary with the tokenized data.
+        """
+        outputs = tokenizer(
+            element[mol_type],
+            truncation=True,
+            max_length=max_length,
+            padding="max_length",
+            add_special_tokens=True,
+        )
+        return {"input_ids": outputs["input_ids"]}
+
+    # Tokenize dataset
+    tokenized_dataset = dataset.map(
+        tokenize_function,
+        batched=True,
+        remove_columns=dataset["train"].column_names,
+        num_proc=datamodule.preprocess_num_workers,
+        load_from_cache_file=not datamodule.overwrite_cache,
+        fn_kwargs={
+            "max_length": datamodule.max_seq_length,
+            "mol_type": datamodule.mol_type,
+            "tokenizer": datamodule.tokenizer,
+        },
+    )
+
+    # Initialize model
     if cfg.model.model_name_or_path == 'gpt2_flash_atten':
-        model = GPT2MolGen_flash_atten(**cfg.model)
+        model = GPT2MolGen_flash_atten(**cfg.model, max_seq_length=datamodule.max_seq_length,
+                                       vocab_size=datamodule.tokenizer.vocab_size)
     elif cfg.model.model_name_or_path in ['llama_small', 'llama_small_HF']:
-        model_cfg = LlamaConfig(**cfg.model)
+        model_cfg = LlamaConfig(**cfg.model, n_ctx=datamodule.max_seq_length,
+                                vocab_size=datamodule.tokenizer.vocab_size)
         model = LlamaForCausalLM(model_cfg)
     elif cfg.model.model_name_or_path == 'llama_small_FA':
-        model = Llama_small_flash_atten(**cfg.model)
+        model = Llama_small_flash_atten(**cfg.model, max_seq_length=datamodule.max_seq_length,
+                                        vocab_size=datamodule.tokenizer.vocab_size)
+    elif cfg.model.model_name_or_path == 'gpt2':
+        model = GPT2MolGen(**cfg.model, max_seq_length=datamodule.max_seq_length,
+                           vocab_size=datamodule.tokenizer.vocab_size)
     else:
-        model = GPT2MolGen(**cfg.model)
+        raise NotImplementedError
 
     # Initialize trainer
-    train_args = MyTrainingArguments(data_seed=cfg.seed, seed=cfg.seed, output_dir=output_dir,
-                                     **cfg.trainer)
-    # enable TF32
-    if torch.cuda.is_available() and cfg.trainer.tf32:
-        torch.backends.cuda.matmul.allow_tf32 = True
-        torch.backends.cudnn.allow_tf32 = True
-
     if cfg.wandb_logs:
         wandb_callback = [WandbCallback(model=model, entity=cfg.wandb.entity, project=cfg.wandb.project,
-                                        name=exp_name, config=OmegaConf.to_container(cfg), tags=cfg.wandb.tags)]
+                                        name=f"{exp_name}_finetune", config=OmegaConf.to_container(cfg), tags=cfg.wandb.tags)]
     else:
         wandb_callback = None
+    output_dir_ = f"{output_dir}_finetune"
+    train_args = MyTrainingArguments(data_seed=cfg.seed, seed=cfg.seed, output_dir=output_dir_, **cfg.trainer)
 
     trainer = MyHFTrainer(model=model,
                           args=train_args,
                           callbacks=wandb_callback,
                           tokenizer=datamodule.tokenizer,
                           data_collator=datamodule.data_collator,
-                          train_dataset=datamodule.train_dataset,
-                          eval_dataset=datamodule.eval_dataset,
+                          train_dataset=tokenized_dataset["train"],
                           evaluation_task=None,
                           )
 
-    # Load checkpoint
-    checkpoint = os.path.join(cfg.save_path, cfg.model_name_or_path)  # "llama_small_FA_PubChem"
-    print(f"load checkpoint from: {checkpoint}")
-    trainer._load_from_checkpoint(checkpoint)
-
-    # Train model
+    print(f"load checkpoint from: {output_dir}")
+    trainer._load_from_checkpoint(output_dir)
     train_result = trainer.train()
-    trainer.save_model()  # Saves the tokenizer too for easy upload
-
-    # if is_world_process_zero(train_args) and isinstance(model, GPTLMHeadModel):
-    #     print('save remapped HF model to:', os.path.join(output_dir, 'HF'))
-    #     model.save_HF_model(OmegaConf.to_container(cfg.model), datamodule.tokenizer,
-    #                         output_dir=os.path.join(output_dir, 'HF'),
-    #                         dataset_name=cfg.dataset['dataset_name'].replace("MolGen/", ""),
-    #                         token=cfg.write_HF_token)
+    trainer.save_model()
 
     # Generate SMILES and calculate metrics
     if isinstance(model, Llama_small_flash_atten) or isinstance(model, GPT2MolGen_flash_atten):
@@ -108,18 +155,12 @@ def entrypoint(cfg: DictConfig):
 
     # Save the SMILES to a CSV file
     df = pd.DataFrame(generated_smiles, columns=["SMILES"])
-    df.to_csv(os.path.join(output_dir, 'generated_smiles.csv'), index=False)
-
-    metrics = get_all_metrics(generated_smiles, n_jobs=cfg.eval.preprocess_num_jobs)
-    # Convert the dictionary to a list of lists
-    metrics_table = [[k, v] for k, v in metrics.items()]
-    print(tabulate(metrics_table, headers=["Metric", "Value"], tablefmt="pretty"))
-
-    df = pd.DataFrame(metrics_table, columns=["Metric", "Value"])
-    df.to_csv(os.path.join(output_dir, 'metrics.csv'), index=False)
-    if cfg.wandb_logs:
-        wandb.log({"evaluation_metrics": wandb.Table(dataframe=df)})
+    pandarallel.initialize(shm_size_mb=60720, nb_workers=args.preprocess_num_jobs, progress_bar=True)
+    df['qed'] = df['SMILES'].parallel_apply(get_qed)
+    df.to_csv(os.path.join(output_dir_, 'generated_smiles.csv'), index=False)
 
 
 if __name__ == "__main__":
-    entrypoint()
+    os.environ['TOKENIZERS_PARALLELISM'] = 'true'
+    args = args_parser()
+    entrypoint(args)
