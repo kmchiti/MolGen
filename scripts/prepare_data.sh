57bc30ba32ae1be93cd197d49c642c8b272fbb22
Kamran Chitsaz <kmchiti@Kamrans-MacBook-Pro.local>
2024-05-21 14:19:36 -0400

    update

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -11,4 +11,4 @@ source ~/anaconda3/bin/activate
 conda activate my-rdkit-env
 export HF_HOME=$SCRATCH/hf_home
 
-python prepare_data.py --config-name=ZINC_22_smiles_atomwise
+python prepare_data.py --config-name=ZINC_270M_smiles_atomwise
