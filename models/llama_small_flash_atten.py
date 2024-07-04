81eb1774f5542afd3aa3774318978bbc9198b937
kmchiti <km.chitsaz@gmail.com>
2024-07-04 13:59:27 -0400

    fix bug in push to hub

--- models/llama_small_flash_atten.py
+++ models/llama_small_flash_atten.py
@@ -1,5 +1,6 @@
 import os.path
 from abc import ABC
+import copy
 from transformers import GPT2Config
 from typing import Optional
 from collections import OrderedDict, namedtuple
@@ -8,6 +9,7 @@ import torch
 from torch.nn import CrossEntropyLoss
 from transformers import LlamaForCausalLM, LlamaConfig
 from flash_attn.models.llama import llama_config_to_gpt2_config, inv_remap_state_dict_hf_llama
+from flash_attn.utils.pretrained import state_dict_from_pretrained
 
 try:
     from flash_attn.models.gpt import GPTLMHeadModel
@@ -63,6 +65,10 @@ class Llama_small_flash_atten(GPTLMHeadModel, ABC):
         config.fused_dropout_add_ln = fused_dropout_add_ln
         config.residual_in_fp32 = residual_in_fp32
         # config.pad_vocab_size_multiple = pad_vocab_size_multiple
+        self.config_copy = copy.deepcopy(config)
+        self.config_copy.max_seq_length = max_seq_length
+        self.config_copy.model_name_or_path = model_name_or_path
+        self.config_copy.intermediate_size = intermediate_size
 
         GPTLMHeadModel.__init__(self, config)
 
@@ -119,17 +125,54 @@ class Llama_small_flash_atten(GPTLMHeadModel, ABC):
             logits=lm_logits,
         )
 
-    def save_HF_model(self, config, tokenizer, max_seq_length, vocab_size, output_dir: str, dataset_name: str = None, token=None):
-        state_dict = self.state_dict()
-        state_dict = {key: value.cpu() for key, value in state_dict.items()}
-        state_dict = inv_remap_state_dict_hf_llama(state_dict, self.config)
+    def save_pretrained(self, save_directory):
+        """Save the model and tokenizer"""
+        self.config_copy.save_pretrained(save_directory)
+        torch.save(self.state_dict(), os.path.join(save_directory, "pytorch_model.bin"))
 
-        model_cfg = LlamaConfig(**config, max_seq_length=max_seq_length, vocab_size=vocab_size)
-        model = LlamaForCausalLM(model_cfg)
+    @classmethod
+    def from_pretrained(cls, load_directory):
+        """Load the model and tokenizer"""
+        config = GPT2Config.from_pretrained(load_directory)
+        model = cls(model_name_or_path=config.model_name_or_path,
+                    max_seq_length=config.max_seq_length,
+                    vocab_size=config.vocab_size,
+                    hidden_size=config.hidden_size,
+                    num_hidden_layers=config.num_hidden_layers,
+                    num_attention_heads=config.num_attention_heads,
+                    intermediate_size=config.intermediate_size,
+                    bos_token_id=config.bos_token_id,
+                    eos_token_id=config.eos_token_id,
+                    use_flash_attn=config.use_flash_attn,
+                    fused_bias_fc=config.fused_bias_fc,
+                    fused_mlp=config.fused_mlp,
+                    fused_dropout_add_ln=config.fused_dropout_add_ln,
+                    residual_in_fp32=config.residual_in_fp32)
+
+        if os.path.exists(load_directory):
+            state_dict = torch.load(os.path.join(load_directory, "pytorch_model.bin"))
+        else:
+            state_dict = state_dict_from_pretrained(load_directory)
         model.load_state_dict(state_dict)
-        model.save_pretrained(output_dir)
-        tokenizer.save_pretrained(output_dir)
+        return model
 
+    def save_HF_model(self, tokenizer, output_dir: str, dataset_name: str = None,
+                      token=None):
+        from huggingface_hub import HfApi
+
+        save_directory = os.path.join(output_dir, 'HF')
+        self.save_pretrained(save_directory)
+        tokenizer.save_pretrained(save_directory)
+
+        # Push to Hugging Face Hub
+        api = HfApi()
         upload_name = f'MolGen/Llama-small-{dataset_name}'
-        model.push_to_hub(upload_name, token=token)
-        tokenizer.push_to_hub(upload_name, token=token)
+        if not api.repo_exists('MolGen/llama_small_FA_ZINC_270M'):
+            api.create_repo(repo_id=upload_name, token=token)
+        api.upload_folder(
+            folder_path=save_directory,
+            path_in_repo=".",
+            repo_id=upload_name,
+            token=token
+        )
+
