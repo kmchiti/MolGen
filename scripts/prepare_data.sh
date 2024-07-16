80f9127cf61dd55d9e0f910840ce363bff36bec3
kmchiti <km.chitsaz@gmail.com>
2024-07-16 14:32:50 -0400

    update config

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=prepare-data-cpu
+#SBATCH --job-name=prepare-data
 #SBATCH --time=3-00:00
-#SBATCH --partition=long-cpu
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=192G
-#SBATCH --ntasks=1
+
+#SBATCH --partition=lab-chandar
+#SBATCH --nodes=1                       # number of nodes
+#SBATCH --gpus-per-task=4               # number of gpus per node
+#SBATCH --cpus-per-task=24              # number of cpus per gpu
+#SBATCH --mem-per-gpu=100G               # memory per gpu
+#SBATCH --ntasks-per-node=1             # crucial - only 1 task per node!
+#SBATCH --signal=TERM@60                # SIGTERM 60s prior to the allocation's end
 
 module load libffi
 source ~/anaconda3/bin/activate
